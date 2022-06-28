a892c194fdb65a34e8cdbc068f9c970b95bcf5c3
Parkmyounghoi <57881372+dron512@users.noreply.github.com>
2022-06-28 16:07:57 +0900

    flask

--- python_work/server.py
+++ python_work/server.py
@@ -14,12 +14,13 @@ db = pymysql.connect(host="localhost", user="root",
                      passwd="1234", db="free_board", charset="utf8")
 cur = db.cursor()
 
-cur.execute('DROP TABLE IF EXISTS aaa')
-cur.execute(''' CREATE TABLE `aaa` (
-  `aa` varchar(45) NOT NULL,
-  `bb` varchar(45) DEFAULT NULL,
-  `filename` varchar(100) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3''')
+# cur.execute('DROP TABLE IF EXISTS aaa')
+# cur.execute(''' CREATE TABLE `aaa` (
+#   `aa` varchar(45) NOT NULL,
+#   `bb` varchar(45) DEFAULT NULL,
+#   `filename` varchar(100) DEFAULT NULL,
+#   `filename_uuid` varchar(100) DEFAULT NULL
+# ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3''')
 
 # 일반적인 라우트 방식입니다.
 
@@ -32,20 +33,38 @@ def insert():
 @app.route('/board/insertproc', methods=['POST'])
 def insertproc():
     print(request.args)
-    aa = request.args.get('aa')
+    aa = request.form['aa']
     bb = request.args.get('bb')
     f = request.files['file']
     #  path = os.path.join(app.config['UPLOAD_DIR'], fname)
     #     f.save(path)
-    myuuid = uuid.uuid1()
-    f.save(f'{myuuid}.{f.filename.split(".")[1]}')
+    myuuid = f'{uuid.uuid1()}.{f.filename.split(".")[1]}'
+    f.save(myuuid)
     try:
-        cur.execute(f"insert into aaa values ('{aa}','{bb}','{f.filename}')")
+        cur.execute(
+            f"insert into aaa values ('{aa}','{bb}','{f.filename}','{myuuid}')")
         db.commit()
     except Exception as e:
         print(e)
     print('잘됨')
     return redirect("/board/index")
+
+# 파일 다운로드 처리
+
+
+@app.route('/fileDown', methods=['GET', 'POST'])
+def down_file():
+    if request.method == 'POST':
+        sw = 0
+        files = os.listdir("./uploads")
+        for x in files:
+            if(x == request.form['file']):
+                sw = 1
+
+        path = "./uploads/"
+        return send_file(path + request.form['file'],
+                         attachment_filename=request.form['file'],
+                         as_attachment=True)
 
 
 @ app.route('/board/index')
