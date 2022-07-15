354055c2d557e7e59afc2671dc0a4a3f17d6484d
dron512 <parkmyounghoi@gmail.com>
2022-07-15 17:14:20 +0900

    220715

--- xls_pyauto_sele_work/selenium/ex03.py
+++ xls_pyauto_sele_work/selenium/ex03.py
@@ -7,6 +7,7 @@ url = './xls_pyauto_sele_work/chromedriver.exe'
 
 web = webdriver.Chrome(url)
 web.get("http://www.naver.com")
+web.maximize_window()
 
 search = web.find_element(By.ID,'query')
 search.send_keys('여행사')
@@ -26,12 +27,40 @@ time.sleep(2)
 
 listelement = web.find_elements(By.CSS_SELECTOR,'.searchLayer .searchLayerInner .travelselectCity dl dd')
 for i in listelement:
-    print(i.text)
     if i.text =='대구':
         i.click()
         break
-    
+time.sleep(3)
+listelement = web.find_elements(By.CSS_SELECTOR,'.searchLayer .searchLayerInner .travelselectCity dl dd')
+for i in listelement:
+    if i.text =='제주':
+        i.click()
+        break
 time.sleep(3)
 
-time.sleep(3)
+listelement = web.find_elements(By.CSS_SELECTOR,'.calBox .calendar td button .day')
+for i in listelement:
+    print(i.text)
+    if i.text =='15':
+        i.click()
+        time.sleep(1)
+        i.click()
+        time.sleep(1)
+    if i.text =='20':
+        i.click()
+        time.sleep(2)
+        break
+        
+elem = web.find_element(By.CSS_SELECTOR,'#rank > div.searchLayerInner > dl:nth-child(1) > dd > div > button.controller.plus')
+elem.click()
+time.sleep(1)
+
+elem = web.find_element(By.CSS_SELECTOR,'#__next > div > div > div.airSearchArea > div.airSearchFormWrap > div > div.filterWrap > form > div.popup.peopNumSel.first.on > div > div.popupFooter > div.confirmBtn > button')
+elem.click()
+time.sleep(1)
+
+elem = web.find_element(By.CSS_SELECTOR,'#__next > div > div > div.airSearchArea > div.airSearchFormWrap > div > div.filterWrap > form > fieldset > div > div.colWrap.peop > div > div.row.sehBtn > button')
+elem.click()
+
+time.sleep(7)
 web.quit()
