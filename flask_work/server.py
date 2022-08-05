da756c0696cc851e5406b3614256624d98d7327f
dron512 <parkmyounghoi@gmail.com>
2022-08-05 15:37:49 +0900

    220805

--- flask_work/server.py
+++ flask_work/server.py
@@ -61,17 +61,19 @@ def test():
     pred2 = '파일을 업로드 하셔야 합니다.'
     knre = ""
     kcl = ""
+    x1=""
+    x2=""
     if request.method == 'GET':
         pass
     elif request.method == 'POST':
         try:
             x_train = np.array([[2, 1],[3, 2],[3, 4],[5, 5],[7, 5],[2, 5],[8, 9],[9, 10],[6, 12]])
-            x_test = np.array([[9, 2],[6, 10],[2, 4]])
             y_train = np.array([3, 5, 7, 10, 12, 7, 13, 13, 12])
-            y_test = np.array([13, 12, 6])
             knr = KNeighborsRegressor(n_neighbors=3)
             knr.fit(x_train,y_train)
             x,y = int(request.form['x0']),int(request.form['x1'])
+            x1=x
+            x2=y
             pred1 = knr.predict([[x,y]])
             pred1 = f'예측하신 타겟값은 = {pred1} 입니다'
             knre ="show"
@@ -88,7 +90,8 @@ def test():
         except Exception as e:
             print(e)
             pred2 = e
-    return render_template("KNeighbors.html", pred1=pred1, pred2=pred2,knre=knre,kcl=kcl)
+    return render_template("KNeighbors.html", 
+            pred1=pred1, pred2=pred2,knre=knre,kcl=kcl,x1=x1,x2=x2)
 
 
 if __name__ == '__main__':
