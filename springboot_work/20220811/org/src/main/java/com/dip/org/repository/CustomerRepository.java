d1dba1d59c447604dbd318702b5ff729cfacf531
dron512 <parkmyounghoi@gmail.com>
2022-08-11 11:24:05 +0900

    220811

---
+++ springboot_work/20220811/org/src/main/java/com/dip/org/repository/CustomerRepository.java
@@ -0,0 +1,13 @@
+package com.dip.org.repository;
+
+import com.dip.org.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends JpaRepository<Customer,Long> {
+    // select * from customer where lasename =?
+    List<Customer> findByLastName(String lastName);
+    // select * from customer where id = ?
+    Customer findById(long id);
+}
