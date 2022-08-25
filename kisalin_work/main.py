10779c024312261cf5bd851ed2b34c2cd7a11059
dron512 <parkmyounghoi@gmail.com>
2022-08-25 15:17:07 +0900

    220825

--- kisalin_work/main.py
+++ kisalin_work/main.py
@@ -1,6 +1,13 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import re
 
-kisa = pd.read_csv('kisa.csv',encoding="euc-kr")
-print(kisa.head())
+# kisa = pd.read_csv('kisa.csv',encoding="euc-kr")
+# print(kisa.head())
 
+pa ="'";
+
+linnames = "['전기공사', '가스', '전자계산기', '신재생에너지발전설비(태양광)', '빅데이터분석']"
+
+a = re.split(r"[']",linnames)
+print(a)
